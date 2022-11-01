72dc8057b8b6ae075d5a4496c43172c0f01dcdf1
Mohibur-CoU-ICT <mohiburrahman5509@gmail.com>
2022-11-01 23:44:27 +0600

    template vs templateUrl

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-users',
-  templateUrl: './users.component.html',
+  template: `
+    <app-user></app-user>
+    <app-user></app-user>
+  `,
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
