a11f5406443099a5739aa7d3d30a247281b9ea95
Mohibur-CoU-ICT <mohiburrahman5509@gmail.com>
2022-11-05 00:09:03 +0600

    custom attribute directive

---
+++ src/app/directives/highlightText.directive.ts
@@ -0,0 +1,15 @@
+import { Directive, OnInit, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlightText]'
+})
+export class HighlightTextDirective implements OnInit {
+
+  constructor(private element: ElementRef) {
+  }
+
+  ngOnInit() {
+    (this.element.nativeElement as HTMLElement).style.background = 'red';
+  }
+
+}
