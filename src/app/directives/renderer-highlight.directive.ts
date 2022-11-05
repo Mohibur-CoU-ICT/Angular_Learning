97d673f1c8b277edbf2dab0f584149c71220c3a2
Mohibur-CoU-ICT <mohiburrahman5509@gmail.com>
2022-11-05 13:09:15 +0600

    sending input data to the directive

--- src/app/directives/renderer-highlight.directive.ts
+++ src/app/directives/renderer-highlight.directive.ts
@@ -1,27 +1,32 @@
-import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appRendererHighlight]'
 })
 export class RendererHighlightDirective implements OnInit {
 
-  @HostBinding('style.backgroundColor') color!: string;
+  @HostBinding('style.backgroundColor') color: string;
+  @Input() mouseOverColor: string = 'blue';
+  @Input() mouseLeaveColor: string = 'green';
+  @Input() clickColor: string = 'red';
 
-  constructor(private element: ElementRef, private renderer: Renderer2) { }
+  constructor(private element: ElementRef, private renderer: Renderer2) {
+    this.color = 'grey';
+  }
 
   ngOnInit(): void {
-    
+
   }
 
   @HostListener('mouseover') onMouseOver() {
-    this.color = 'blue';
+    this.color = this.mouseOverColor;
   }
 
   @HostListener('mouseleave') onMouseLeave() {
-    this.color = 'green';
+    this.color = this.mouseLeaveColor;
   }
   
   @HostListener('click') onClick() {
-    this.color = 'red';
+    this.color = this.clickColor;
   }
 }
