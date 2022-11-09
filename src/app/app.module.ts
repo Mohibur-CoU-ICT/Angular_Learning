11dcb1cb65b777f40edf2740655349544a6140f8
Mohibur-CoU-ICT <mohiburrahman5509@gmail.com>
2022-11-09 21:48:21 +0600

    generate home, users, categories, roles components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { RolesComponent } from './roles/roles.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    UsersComponent,
+    RolesComponent,
   ],
   imports: [
     BrowserModule,
