796204604e8db1b6331ea116ce445dfe3f571b4b
Mohibur-CoU-ICT <mohiburrahman5509@gmail.com>
2022-11-09 22:07:13 +0600

    setup & load routes with RouterModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,14 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
 import { RolesComponent } from './roles/roles.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'categories', component: CategoriesComponent },
+];
 
 @NgModule({
   declarations: [
@@ -13,10 +21,12 @@ import { RolesComponent } from './roles/roles.component';
     HomeComponent,
     UsersComponent,
     RolesComponent,
+    CategoriesComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [
   ],
