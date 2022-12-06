de5c650b66e5ea8d597fd5a41aff99cbc7296391
Mohibur-CoU-ICT <mohiburrahman5509@gmail.com>
2022-12-07 00:27:32 +0600

    routing configuration in app-routing.module.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,25 +7,10 @@ import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
 import { RolesComponent } from './roles/roles.component';
 import { CategoriesComponent } from './categories/categories.component';
-import { Routes, RouterModule } from '@angular/router';
 import { UserComponent } from './user/user.component';
 import { EditUserComponent } from './edit-user/edit-user.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
-const routes: Routes = [
-  { path: '', component: HomeComponent },
-  {
-    path: 'users',
-    component: UsersComponent,
-    children: [
-      { path: ':id/:name', component: UserComponent },
-      { path: ':id/:name/edit', component: EditUserComponent }
-    ],
-  },
-  { path: 'categories', component: CategoriesComponent },
-  { path: 'not-found', component: PageNotFoundComponent },
-  { path: '**', redirectTo: 'not-found' },
-];
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -40,8 +25,8 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     FormsModule,
-    RouterModule.forRoot(routes),
   ],
   providers: [
   ],
