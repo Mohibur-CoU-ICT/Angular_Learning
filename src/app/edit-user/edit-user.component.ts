bc737cef6552af1c0cf7a73b77a9cb4d87e2d8b7
Mohibur-CoU-ICT <mohiburrahman5509@gmail.com>
2022-12-15 00:22:34 +0600

    canDeactive route guard in real time scenario

--- src/app/edit-user/edit-user.component.ts
+++ src/app/edit-user/edit-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Observable } from 'rxjs';
 import { IDeactiveGuard } from '../services/guards/deactivate-guard.service';
 
@@ -9,16 +10,36 @@ import { IDeactiveGuard } from '../services/guards/deactivate-guard.service';
 })
 export class EditUserComponent implements OnInit, IDeactiveGuard {
 
-  constructor() { }
+  user!: { id: string, name: string };
+
+  editUserDetails!: { id: string, name: string };
+
+  constructor(private route: ActivatedRoute) {
+
+  }
 
   ngOnInit(): void {
+    this.route.params.subscribe((data: Params) => {
+      this.user = {
+        id: data['id'],
+        name: data['name']
+      };
+
+      this.editUserDetails = { ...this.user };
+    });
   }
 
   canExit(): boolean | Promise<boolean> | Observable<boolean> {
-    if (confirm('Are are sure you want to exit?')) {
-      return true;
+    if (this.user.id !== this.editUserDetails.id || this.user.name !== this.editUserDetails.name) {
+      if (confirm('All unsaved changes will be lost')) {
+        return true;
+      }
+      else {
+        return false;
+      }
     }
-    return false;
+
+    return true;
   }
 
 }
