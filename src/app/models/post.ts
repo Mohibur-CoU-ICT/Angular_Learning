d6efd8a5867068bee357cf00ca2e8cde729b04b1
Mohibur-CoU-ICT <mohiburrahman5509@gmail.com>
2023-01-05 00:08:52 +0600

    http get request and use rxjs operators

---
+++ src/app/models/post.ts
@@ -0,0 +1,5 @@
+export interface Post {
+  key: string;
+  title: string;
+  content: string;
+}
--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { map } from 'rxjs';
+import { Post } from '../models/post';
 
 @Component({
   selector: 'app-posts',
@@ -11,6 +13,7 @@ export class PostsComponent implements OnInit {
 
   postForm: FormGroup;
   url: string;
+  posts: Post[] = [];
 
   constructor(private http: HttpClient) {
     this.url = 'https://angular-learning-10aec-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json';
@@ -21,12 +24,33 @@ export class PostsComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.getPosts();
+  }
+
+  getPosts() {
+    this.http.get(this.url).pipe(map((response) => {
+      let posts: Post[] = [];
+      for (let k of Object.entries(response)) {
+        // console.log(k);
+        let post: Post = {
+          key: k[0],
+          title: k[1].title,
+          content: k[1].content
+        };
+        posts.push(post);
+      }
+      return posts;
+    })).subscribe((response) => {
+      // console.log(response);
+      this.posts = response;
+    });
   }
 
   onPostAdded() {
     let postData = this.postForm.value;
     this.http.post(this.url, postData).subscribe((response) => {
       console.log(response);
+      this.getPosts();
     });
   }
 
