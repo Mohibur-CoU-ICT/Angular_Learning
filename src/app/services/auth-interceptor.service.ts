c0fc92f9e6d880d89c8f37966ec2df50cfa76834
Mohibur-CoU-ICT <mohiburrahman5509@gmail.com>
2023-06-29 20:07:40 +0600

    accessing http response event object with response interceptor

--- src/app/services/auth-interceptor.service.ts
+++ src/app/services/auth-interceptor.service.ts
@@ -1,16 +1,29 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { Observable } from "rxjs";
+import {
+  HttpEvent,
+  HttpEventType,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
 
 export class AuthInterceptorService implements HttpInterceptor {
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    // to find the url
-    // if (req.url == '')
-    let modifiedRequest =  req.clone({
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    let modifiedRequest = req.clone({
       headers: req.headers.append('auth', 'abc'),
       params: req.params.append('hai', 'hello world'),
     });
-    console.log('Sending request interceptor');
-    return next.handle(modifiedRequest);
+    return next.handle(modifiedRequest).pipe(
+      tap((event) => {
+        console.log('Response from http interceptor');
+        console.log(event);
+        if (event.type === HttpEventType.Response) {
+          console.log(event.body);
+        }
+      })
+    );
   }
-
 }
